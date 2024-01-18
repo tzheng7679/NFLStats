d9555639cae57602458ffdb177e66313cb686a33
trollol7679 <trollol7679@gmail.com>
2024-01-18 16:28:59 -0500

    Temporary end of day changes

--- app/src/main/java/com/example/nflstats/MainActivity.kt
+++ app/src/main/java/com/example/nflstats/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.nflstats
 
+import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,4 +36,21 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+@Preview
+fun LogoMaker(modifier : Modifier = Modifier) {
+    val ari = painterResource(R.drawable.ari)
+
+    Box {
+        Image(painter = ari, contentDescription = null)
+        TeamText(modifier = Modifier.fillMaxSize().padding(8.dp))
+    }
+}
+
+@Composable
+@Preview
+fun TeamText(modifier : Modifier = Modifier) {
+    Text("Arizona Cardinals", modifier = modifier, fontSize = 50.sp, color = MaterialTheme.colorScheme.primary)
 }
