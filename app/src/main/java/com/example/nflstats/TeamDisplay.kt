9003a534b318e16fb61a2c3a9b61dd287bddf91e
trollol7679 <trollol7679@gmail.com>
2024-01-25 12:22:40 -0500

    Added functionality for a button to get a random logo; example code for later when developing actual team display activity

--- app/src/main/java/com/example/nflstats/TeamDisplay.kt
+++ app/src/main/java/com/example/nflstats/TeamDisplay.kt
@@ -4,18 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 //maps city abb. to team logos
 val teamImageMap = mapOf(
@@ -62,24 +71,64 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                ////////////////////////////
+                TeamDisplayApp()
             }
         }
     }
-}
-@Composable
-@Preview(showBackground = true)
-fun LogoMaker(modifier : Modifier = Modifier, city : String = "ari") {
-    val ari = painterResource(teamImageMap[city]!!)
 
-    Box(modifier = modifier) {
-        Image(painter = ari, contentDescription = null)
-        TeamText(modifier = Modifier.fillMaxSize().padding(8.dp))
+    /*
+    @Composable
+    @Preview(showBackground = true)
+    fun LogoMaker(modifier : Modifier = Modifier, city : String = "ari") {
+        val ari = painterResource(teamImageMap[city]!!)
+
+        Row(modifier = modifier) {
+            Image(painter = ari, contentDescription = null)
+            TeamText(city = city, modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize())
+        }
     }
-}
 
-@Composable
-@Preview
-fun TeamText(modifier : Modifier = Modifier) {
-    Text("Arizona Cardinals", modifier = modifier, fontSize = 50.sp, color = MaterialTheme.colorScheme.primary)
+    @Composable
+    fun TeamText(city : String, modifier : Modifier = Modifier) {
+        Text(text = getString(getResources().getIdentifier("city", "drawable", null)),
+            modifier = modifier,
+            fontSize = 50.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            maxLines = 2
+        )
+    }
+    */
+    @Composable
+    @Preview(showBackground = true)
+    fun TeamDisplayApp() {
+        TeamWithButton(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center))
+    }
+
+    @Composable
+    fun TeamWithButton(modifier : Modifier = Modifier) {
+        var team by remember {mutableStateOf(R.drawable.ari)}
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Image(painter = painterResource(team), "", modifier = Modifier.size(150.dp))
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = {
+                team = teamImageMap.values.random()
+
+            }) {
+                Text("switch team")
+            }
+
+        }
+
+    }
 }
